ace0e583c0c4b642bcf861119837760d817b876b
Alexander Timutsa <a.timutsa@gmail.com>
2016-03-31 00:56:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        navigation: {
+            'position': 'right',
+            'tooltips': ['Главная', 'Page 2', 'Page 3', 'Pgae 4']
+        }
+    });
+});
